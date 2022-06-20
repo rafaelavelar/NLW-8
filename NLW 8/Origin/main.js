8b6e45970950445f034970c5edfb127a0394d0f3
rafaelavelar <ra.fa89@hotmail.com>
2022-06-20 11:37:17 -0300

    update responsividade desktop

--- NLW 8/Origin/main.js
+++ NLW 8/Origin/main.js
@@ -4,7 +4,49 @@ onScroll()
 function onScroll() {
   showNavOnScroll()
   showBackToTopButton()
+
+  
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
 }
+
+function activateMenuAtCurrentSection (section) {
+  const targetLine = scrollY + innerHeight / 2
+
+  //verificar se a seção passou da linha
+  //quais dados vou precisar?
+
+  //o topo da seção
+  const sectionTop = section.offsetTop
+  //a altura da seção
+  const sectionHeight = section.offsetHeight
+  //o topo da seção chegou ou ultrapassou a linha alvo
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  //verificar se a base está abaixo da linha alvo
+  //quais dados vou precisar?
+  //a seção termina onde?
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  //o final da seção passou da linha alvo
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  //limites da seção
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`) 
+  
+  menuElement.classList.remove('active')
+  if(sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+
+} 
+
 
 function showNavOnScroll() {
   const navigation = document.getElementById('navigation')
